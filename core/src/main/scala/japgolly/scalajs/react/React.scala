b98724be476009580595d894987cebea0bfff075
David Barri <japgolly@gmail.com>
2014-11-23 10:23:17 +1100

    Updated React top-level API to React 0.12

--- core/src/main/scala/japgolly/scalajs/react/React.scala
+++ core/src/main/scala/japgolly/scalajs/react/React.scala
@@ -20,20 +20,10 @@ object React extends Object {
   def createElement[P,S,B,N <: TopNode](t: ReactComponentType[P,S,B,N]): ReactComponentCU[P,S,B,N] = ???
   def createElement(tag: String, props: Object, children: ReactNode*): ReactDOMElement = ???
 
-  def renderComponent(c: ReactComponentU_, n: dom.Node)
-    : ReactComponentM_[TopNode] = ???
-
-  def renderComponent(c: ReactComponentU_, n: dom.Node, callback: ThisFunction)
-    : ReactComponentM_[TopNode] = ???
-
-  def renderComponent[P,S,B,N <: TopNode](c: ReactComponentU[P,S,B,N], n: dom.Node)
-    : ReactComponentM[P,S,B,N] = ???
-
-  def renderComponent[P,S,B,N <: TopNode](c: ReactComponentU[P,S,B,N], n: dom.Node, callback: ThisFunction0[ReactComponentM[P,S,B,N], Unit])
-    : ReactComponentM[P,S,B,N] = ???
-
-  /** Configure React's event system to handle touch events on mobile devices. */
-  def initializeTouchEvents(shouldUseTouch: Boolean): Unit = ???
+  def render(e: ReactElement, n: dom.Node): ReactComponentM_[TopNode] = ???
+  def render(e: ReactElement, n: dom.Node, callback: ThisFunction): ReactComponentM_[TopNode] = ???
+  def render[P,S,B,N <: TopNode](c: ReactComponentU[P,S,B,N], n: dom.Node): ReactComponentM[P,S,B,N] = ???
+  def render[P,S,B,N <: TopNode](c: ReactComponentU[P,S,B,N], n: dom.Node, callback: ThisFunction0[ReactComponentM[P,S,B,N], Unit]): ReactComponentM[P,S,B,N] = ???
 
   /**
    * Remove a mounted React component from the DOM and clean up its event handlers and state. If no component was
@@ -42,24 +32,75 @@ object React extends Object {
    */
   def unmountComponentAtNode(container: dom.Node): Boolean = ???
 
-  def renderComponentToString(component: ReactComponentU_): String = ???
+  /**
+   * Render a ReactElement to its initial HTML. This should only be used on the server. React will return an HTML
+   * string. You can use this method to generate HTML on the server and send the markup down on the initial request for
+   * faster page loads and to allow search engines to crawl your pages for SEO purposes.
+   *
+   * If you call React.render() on a node that already has this server-rendered markup, React will preserve it and only
+   * attach event handlers, allowing you to have a very performant first-load experience.
+   */
+  def renderToString(e: ReactElement): String = ???
 
-  def renderComponentToStaticMarkup(component: ReactComponentU_): String = ???
+  /**
+   * Similar to renderToString, except this doesn't create extra DOM attributes such as data-react-id, that React uses
+   * internally. This is useful if you want to use React as a simple static page generator, as stripping away the extra
+   * attributes can save lots of bytes.
+   */
+  def renderToStaticMarkup(e: ReactElement): String = ???
 
+  /** Verifies the object is a ReactElement. */
+  def isValidElement(o: JAny): Boolean = ???
+
+  /** Configure React's event system to handle touch events on mobile devices. */
+  def initializeTouchEvents(shouldUseTouch: Boolean): Unit = ???
+
+  /**
+   * React.DOM provides convenience wrappers around React.createElement for DOM components. These should only be used
+   * when not using JSX. For example, React.DOM.div(null, 'Hello World!')
+   */
   def DOM: Dynamic = ???
+
   def addons: Dynamic = ???
 
+  /** React.Children provides utilities for dealing with the this.props.children opaque data structure. */
   def Children: ReactChildren = ???
+
+  @deprecated("React.renderComponent will be deprecated in a future version. Use React.render instead.", "React 0.12.0")
+  def renderComponent(c: ReactComponentU_, n: dom.Node): ReactComponentM_[TopNode] = ???
+
+  @deprecated("React.renderComponent will be deprecated in a future version. Use React.render instead.", "React 0.12.0")
+  def renderComponent(c: ReactComponentU_, n: dom.Node, callback: ThisFunction): ReactComponentM_[TopNode] = ???
+
+  @deprecated("React.renderComponent will be deprecated in a future version. Use React.render instead.", "React 0.12.0")
+  def renderComponent[P, S, B, N <: TopNode](c: ReactComponentU[P, S, B, N], n: dom.Node): ReactComponentM[P, S, B, N] = ???
+
+  @deprecated("React.renderComponent will be deprecated in a future version. Use React.render instead.", "React 0.12.0")
+  def renderComponent[P, S, B, N <: TopNode](c: ReactComponentU[P, S, B, N], n: dom.Node, callback: ThisFunction0[ReactComponentM[P, S, B, N], Unit]): ReactComponentM[P, S, B, N] = ???
+
+  @deprecated("React.renderComponentToString will be deprecated in a future version. Use React.renderToString instead.", "React 0.12.0")
+  def renderComponentToString(component: ReactComponentU_): String = ???
+
+  @deprecated("React.renderComponentToStaticMarkup will be deprecated in a future version. Use React.renderToStaticMarkup instead.", "React 0.12.0")
+  def renderComponentToStaticMarkup(component: ReactComponentU_): String = ???
 }
 
 /** `React.Children` */
 trait ReactChildren extends Object {
+  /** Invoke fn on every immediate child contained within children with this set to context. If children is a nested object or array it will be traversed: fn will never be passed the container objects. If children is null or undefined returns null or undefined rather than an empty object. */
   def map(c: PropsChildren, fn: js.Function1[ReactNode, JAny]): UndefOr[Object] = ???
+  /** Invoke fn on every immediate child contained within children with this set to context. If children is a nested object or array it will be traversed: fn will never be passed the container objects. If children is null or undefined returns null or undefined rather than an empty object. */
   def map(c: PropsChildren, fn: js.Function2[ReactNode, Number, JAny]): UndefOr[Object] = ???
+
+  /** Like React.Children.map() but does not return an object. */
   def forEach(c: PropsChildren, fn: js.Function1[ReactNode, JAny]): Unit = ???
+  /** Like React.Children.map() but does not return an object. */
   def forEach(c: PropsChildren, fn: js.Function2[ReactNode, Number, JAny]): Unit = ???
-  /** WARNING: Throws an exception is exact number of children is not 1. */
+
+  /** Return the only child in children. Throws otherwise. */
   def only(c: PropsChildren): ReactNode = ???
+
+  /** Return the total number of components in children, equal to the number of times that a callback passed to map or forEach would be invoked. */
   def count(c: PropsChildren): Number = ???
 }
 
